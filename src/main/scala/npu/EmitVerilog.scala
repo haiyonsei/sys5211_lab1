add1f4bd6da388a9b559b03d987ba2031ed665e2
PMJ <134495473+JackPark2000@users.noreply.github.com>
2025-09-28 18:38:23 +0900

    Update EmitVerilog.scala

--- src/main/scala/npu/EmitVerilog.scala
+++ src/main/scala/npu/EmitVerilog.scala
@@ -4,36 +4,7 @@ package npu
 import chisel3.stage.ChiselGeneratorAnnotation
 import circt.stage.ChiselStage
 
-// Verilog를 뽑고 싶을 때 편리한 object
 object EmitVerilog extends App {
     (new chisel3.stage.ChiselStage).emitVerilog(new NpuExeTop(
           blockSize = 4, spBanks = 4, spEntries = 256, dataW = 16, addrW = 16), args)
 }
-
-
-/*
-package npu
-
-import chisel3.stage.ChiselGeneratorAnnotation
-import circt.stage.ChiselStage
-
-object EmitVerilog extends App {
-  (new ChiselStage).execute(
-    Array(
-      "--target", "systemverilog",       // CIRCT 백엔드로 SV 생성
-      "--target-dir", "generated",
-      "--split-verilog",
-      "--disable-all-randomization"      // 재현성 원하면 유지, 아니면 제거
-      // 필요시 추가: "--preserve-aggregate=1"
-    ),
-    Seq(
-      ChiselGeneratorAnnotation(() =>
-        new NpuExeTop(
-          blockSize = 4, spBanks = 4, spEntries = 256, dataW = 16, addrW = 16
-        )
-      )
-    )
-  )
-}
-
-*/
